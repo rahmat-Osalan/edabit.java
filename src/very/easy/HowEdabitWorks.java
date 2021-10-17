8e56aa7695f904a7998e5f0e4b98f3cbf981e2a4
Egor Vorobey <faIlen.exorcism@gmail.com>
2021-10-17 11:12:57 +0300

    add HowEdabitWorks.java

---
+++ src/very/easy/HowEdabitWorks.java
@@ -0,0 +1,9 @@
+// https://edabit.com/challenge/L7z5EW4MSoGBi6cXP
+
+package very.easy;
+
+public class HowEdabitWorks {
+    public static boolean returnTrue() {
+        return true;
+    }
+}
---
+++ testing/very/easy/test/HowEdabitWorksT.java
@@ -0,0 +1,13 @@
+package very.easy.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+import very.easy.HowEdabitWorks;
+
+public class HowEdabitWorksT {
+    @Test
+    public void test1(){
+        assertTrue(HowEdabitWorks.returnTrue());
+    }
+}
